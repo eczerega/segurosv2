c2272fb4e655b8480a3f6d683e3521ad398c41b5
eczerega <eczerega@uc.cl>
2015-06-13 00:19:22 -0400

    folder

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,13 @@ Rails.application.routes.draw do
 
   resources :usuarios
 
+  resources :users do
+    member do
+      get :download
+    end
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
