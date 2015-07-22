8c99e5f8b126d0ee62a38de67198e16c6175014d
eczerega <eczerega@uc.cl>
2015-07-21 21:43:55 -0400

    imagenes en heroku no funcionan

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,24 @@
 class SessionsController < ApplicationController
 
-
+  $status=0
+  $help='asd'
   def new
+    $status
   end
 
   def create
     u = Usuario.find_by(email: params[:session][:email])
-
+    $status=10
+    $help = u.nombre
     if u.try(:authenticate, params[:session][:password])
-
+      $status=2
       # login successfull!
       session[:usuario_id] = u.id
       # flash.notice = "Ingreso exitoso"
       # o usemos la opción de redirect_to, que es equivalente
       redirect_to root_path, notice: "Ingreso exitoso"
     else
+      $status=3
       flash.now.alert = "Email o contraseña inválidos"
       render :new
     end
