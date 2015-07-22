47c34781beacf101018fbe9ccf4f0c36cc68c031
eczerega <eczerega@uc.cl>
2015-07-21 22:00:28 -0400

    asd

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+Bundler.require(*Rails.groups)heroku 
 module Seguros
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
