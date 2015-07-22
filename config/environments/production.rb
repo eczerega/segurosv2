9926f1eae6f3fc6be6dbc72815a659c0d2f9804f
eczerega <eczerega@uc.cl>
2015-07-21 22:36:02 -0400

    asd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,6 @@ Rails.application.configure do
   config.log_level = :debug
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
-  config.log_formatter = ::Logger::Formatter.ne
+  config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
 end
